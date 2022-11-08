d0927b14f4abca0b375823dbc01ccbb4f745cef4
Koushik-Deb <koushik.imon@gmail.com>
2022-11-08 16:21:59 +0600

    Fix Bug and Add required Node version dialogue

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -1,22 +1,43 @@
-import { it, expect } from "vitest";
-import { transformToNumber } from "./numbers";
+import { describe, it, expect } from "vitest";
+import { cleanNumbers, transformToNumber } from "./numbers";
 
-//you can separate tests rather than keep in one test
-it("should transform a string number to a number of type number", () => {
-  const input = "1";
+describe("transformToNumber", () => {
+  it("should transform a string number to a number of type number", () => {
+    const input = "1";
 
-  const result = transformToNumber(input);
+    const result = transformToNumber(input);
 
-  expect(result).toBe(+input);
-  expect(result).toBeTypeOf("number");
+    expect(result).toBe(+input);
+    expect(result).toBeTypeOf("number");
+  });
+
+  it("should yield NaN for non-transformable values", () => {
+    const input = "invalid";
+    const input2 = {};
+
+    const result = transformToNumber(input);
+    const result2 = transformToNumber(input2);
+    expect(result).toBeNaN();
+    expect(result2).toBeNaN();
+  });
 });
 
-it("should yield NaN for non-transformable values", () => {
-  const input = "invalid";
-  const input2 = {};
+describe("cleanNumbers()", () => {
+  it("should return an array of number values if an array of string number values is provided", () => {
+    const numberValues = ["1", "2"];
 
-  const result = transformToNumber(input);
-  const result2 = transformToNumber(input2);
-  expect(result).toBeNaN();
-  expect(result2).toBeNaN();
+    const cleanedNumbers = cleanNumbers(numberValues);
+
+    expect(cleanedNumbers[0]).toBeTypeOf("number");
+  });
+
+  it("should throw an error if an array with at least one empty string is provided", () => {
+    const numberValues = ["", 1];
+
+    const clearFn = () => {
+      cleanNumbers(numberValues);
+    };
+
+    expect(clearFn).toThrow();
+  });
 });
